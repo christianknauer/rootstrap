ec12433b37f38edae6f7987318932b640bff6239
Christian Knauer <email@christianknauer.de>
2023-09-04 13:58:52 +0200

    Run rootstrap code from source host.

---
+++ source/run_rootstrap.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# run_rootstrap.sh 
+
+# run root bootstrap code on target
+
+# init 
+
+source config.global
+source tools
+
+SCRIPT=`basename "$0"`
+
+USAGE="\"$SCRIPT hostname\""
+
+Begin run_rootstrap
+
+if [ -z ${1+x} ]; then ErrorMsg "no target given, usage is $USAGE"; exit 1; fi;               TARGET_SPEC=$1
+
+InfoMsg "run root bootstrap code on target $TARGET_SPEC"
+
+InfoMsg "cloning admin bootstrap code to target"
+
+ssh root@$TARGET_SPEC "git clone https://github.com/christianknauer/rootstrap.git"
+if [ ! $? == 0 ]; then
+    ErrorMsg "could not clone rootstrap code"; exit 1;
+fi
+
+InfoMsg "run root bootstrap code on target"
+ssh root@$TARGET_SPEC ". rootstrap/rootstrap.sh"
+
+# EOF
