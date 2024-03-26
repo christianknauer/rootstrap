903e5bc124987ffdc8217282c7195bd6de07ec4a
Christian Knauer <email@christianknauer.de>
2024-03-26 17:24:14 +0100

    setup

--- nexgen/payload.sh
+++ nexgen/payload.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-sudo SSH_PUBLIC_KEY="${SSH_PUBLIC_KEY:-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH5ybW/0rquFoLSBtMrFytPH75MbTj0uS3WKD/I7OjHu admin (during setup only)}" USERNAME="$USER" bash <<"EOF"
+sudo SSH_PUBLIC_KEY="${1:-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH5ybW/0rquFoLSBtMrFytPH75MbTj0uS3WKD/I7OjHu admin (during setup only)}" USERNAME="$USER" bash <<"EOF"
 
 echo "APT::Get::Always-Include-Phased-Updates True;" > /etc/apt/apt.conf.d/99-Phased-Updates
 
